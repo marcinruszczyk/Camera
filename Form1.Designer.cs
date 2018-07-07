00c210302ef493de662f0ded89ff6468266cf8e9
Marcin <marcin.ruszczyk@wp.pl>
2018-07-07 16:15:21 +0200

    simple camera

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace CameraForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbCameraDevices = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbCameraResolution = new System.Windows.Forms.ComboBox();
+            this.savebtn = new System.Windows.Forms.Button();
+            this.picCamera = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picCamera)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbCameraDevices
+            // 
+            this.cmbCameraDevices.FormattingEnabled = true;
+            this.cmbCameraDevices.Location = new System.Drawing.Point(200, 34);
+            this.cmbCameraDevices.Name = "cmbCameraDevices";
+            this.cmbCameraDevices.Size = new System.Drawing.Size(375, 24);
+            this.cmbCameraDevices.TabIndex = 0;
+            this.cmbCameraDevices.SelectedIndexChanged += new System.EventHandler(this.cmbCameraDevices_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Camera devices";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Resolutions";
+            // 
+            // cmbCameraResolution
+            // 
+            this.cmbCameraResolution.FormattingEnabled = true;
+            this.cmbCameraResolution.Location = new System.Drawing.Point(200, 101);
+            this.cmbCameraResolution.Name = "cmbCameraResolution";
+            this.cmbCameraResolution.Size = new System.Drawing.Size(375, 24);
+            this.cmbCameraResolution.TabIndex = 3;
+            this.cmbCameraResolution.SelectedIndexChanged += new System.EventHandler(this.cmbCameraResolution_SelectedIndexChanged);
+            // 
+            // savebtn
+            // 
+            this.savebtn.Location = new System.Drawing.Point(248, 463);
+            this.savebtn.Name = "savebtn";
+            this.savebtn.Size = new System.Drawing.Size(75, 23);
+            this.savebtn.TabIndex = 4;
+            this.savebtn.Text = "Save";
+            this.savebtn.UseVisualStyleBackColor = true;
+            this.savebtn.Click += new System.EventHandler(this.savebtn_Click);
+            // 
+            // picCamera
+            // 
+            this.picCamera.Location = new System.Drawing.Point(79, 173);
+            this.picCamera.Name = "picCamera";
+            this.picCamera.Size = new System.Drawing.Size(583, 255);
+            this.picCamera.TabIndex = 5;
+            this.picCamera.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 536);
+            this.Controls.Add(this.picCamera);
+            this.Controls.Add(this.savebtn);
+            this.Controls.Add(this.cmbCameraResolution);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbCameraDevices);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.picCamera)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbCameraDevices;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbCameraResolution;
+        private System.Windows.Forms.Button savebtn;
+        private System.Windows.Forms.PictureBox picCamera;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DarrenLee.Media;
+
+
+namespace CameraForms
+{
+    public partial class Form1 : Form
+    {
+
+        int count = 0;
+        Camera myCamera = new Camera();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            GetInfo();
+            myCamera.OnFrameArrived += MyCamera_OnFrameArrived;
+
+        }
+
+        private void GetInfo()
+        {
+            var cameraDevices = myCamera.GetCameraSources();
+            var cameraResolutions = myCamera.GetSupportedResolutions();
+
+
+
+            foreach (var d in cameraDevices)
+                cmbCameraDevices.Items.Add(d);
+
+
+            foreach (var r in cameraResolutions)
+                cmbCameraResolution.Items.Add(r);
+            
+                cmbCameraResolution.SelectedIndex = 0;
+                cmbCameraDevices.SelectedIndex = 0;
+          }
+
+
+
+        private void MyCamera_OnFrameArrived(object source, FrameArrivedEventArgs e)
+        {
+            Image img = e.GetFrame();
+            picCamera.Image = img;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbCameraDevices_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            myCamera.ChangeCamera(cmbCameraDevices.SelectedIndex);
+        }
+
+        private void cmbCameraResolution_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            myCamera.Start(cmbCameraResolution.SelectedIndex);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            myCamera.Stop();
+        }
+
+        private void savebtn_Click(object sender, EventArgs e)
+        {
+            string filename = Application.StartupPath + @"\" + "Image" + count.ToString();
+            myCamera.Capture(filename);
+            count++;
+        }
+    }
+}
